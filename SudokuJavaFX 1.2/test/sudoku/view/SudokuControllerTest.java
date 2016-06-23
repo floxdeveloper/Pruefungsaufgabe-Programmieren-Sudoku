c3d423ddf56ddd65b893babd024949c0db47bd0a
floxdeveloper <fritz66@alpenjodel.de>
2016-06-24 00:34:38 +0200

    A first GUI Test with TestFX for the SudokuController and the Sudoku UI.

--- SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
+++ SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
@@ -1,5 +1,28 @@
 package sudoku.view;
 
-public class SudokuControllerTest {
+import java.io.IOException;
 
+import org.junit.Test;
+import org.loadui.testfx.GuiTest;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class SudokuControllerTest extends GuiTest{
+	protected Parent getRootNode() {
+        Parent parent = null;
+        try {
+            parent = FXMLLoader.load(getClass().getResource("Sudoku.fxml"));
+            return parent;
+        } catch (IOException ex) {
+           System.err.println("Es konnte kein Parent aus der Sudoku.fxml gezogen werden");
+        }
+        return parent;
+    }
+	
+	@Test
+	public void markCell() {
+		RectPos rect10 = find("#r10");
+		click(rect10);
+	}
 }
