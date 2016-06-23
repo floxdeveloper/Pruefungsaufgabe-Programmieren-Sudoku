ef9a577a77824991c627d0e1ee3a64f72843ac78
floxdeveloper <fritz66@alpenjodel.de>
2016-06-24 00:35:25 +0200

    Initialize a JUnit Test Class for the InputNumberPopupController

---
+++ SudokuJavaFX 1.2/test/sudoku/view/InputNumberPopupControllerTest.java
@@ -0,0 +1,16 @@
+package sudoku.view;
+
+import org.loadui.testfx.GuiTest;
+
+import javafx.scene.Parent;
+
+public class InputNumberPopupControllerTest extends GuiTest {
+
+	@Override
+	protected Parent getRootNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
