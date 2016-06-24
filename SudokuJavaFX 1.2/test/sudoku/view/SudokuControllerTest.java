76113b68b348a8d7f45c574f867194a5d816d164
floxdeveloper <fritz66@alpenjodel.de>
2016-06-24 17:34:51 +0200

    Move to SudokuGuiTest

--- SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
+++ SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
@@ -1,28 +1,5 @@
 package sudoku.view;
 
-import java.io.IOException;
-
-import org.junit.Test;
-import org.loadui.testfx.GuiTest;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-
-public class SudokuControllerTest extends GuiTest{
-	protected Parent getRootNode() {
-        Parent parent = null;
-        try {
-            parent = FXMLLoader.load(getClass().getResource("Sudoku.fxml"));
-            return parent;
-        } catch (IOException ex) {
-           System.err.println("Es konnte kein Parent aus der Sudoku.fxml gezogen werden");
-        }
-        return parent;
-    }
+public class SudokuControllerTest{
 	
-	@Test
-	public void markCell() {
-		RectPos rect10 = find("#r10");
-		click(rect10);
-	}
 }
