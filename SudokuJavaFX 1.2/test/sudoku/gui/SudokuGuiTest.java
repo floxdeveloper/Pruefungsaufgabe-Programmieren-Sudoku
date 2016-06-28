b6c8160d6b2b5ecd653ded6e9e060fb428788b1a
floxdeveloper <fritz66@alpenjodel.de>
2016-06-28 12:57:48 +0200

    New Layout needs rootLayout

--- SudokuJavaFX 1.2/test/sudoku/gui/SudokuGuiTest.java
+++ SudokuJavaFX 1.2/test/sudoku/gui/SudokuGuiTest.java
@@ -7,9 +7,12 @@ import org.loadui.testfx.GuiTest;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import sudoku.MainApp;
 import sudoku.view.RectPos;
 
 public class SudokuGuiTest extends GuiTest{
+	AnchorPane rootLayout;
 	
 	/*The widgets of the gui used for the tests */
 	RectPos rect10;
@@ -17,8 +20,11 @@ public class SudokuGuiTest extends GuiTest{
 	protected Parent getRootNode() {
         Parent parent = null;
         try {
-            parent = FXMLLoader.load(getClass().getResource("Sudoku.fxml"));
-            return parent;
+        	FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/Sudoku.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+			parent = rootLayout;
+			return parent;
         } catch (IOException ex) {
            System.err.println("Es konnte kein Parent aus der Sudoku.fxml gezogen werden");
         }
