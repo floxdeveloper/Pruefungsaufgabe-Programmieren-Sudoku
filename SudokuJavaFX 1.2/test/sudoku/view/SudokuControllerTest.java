59ef9cb11e634355c4a4eb46f599cb6fdbdc8cd7
Zenty <yvette.labastille@gmail.com>
2016-07-05 11:30:24 +0200

    Copy all changes

--- SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
+++ SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
@@ -3,46 +3,40 @@ package sudoku.view;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
 
 import javafx.application.Application;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import sudoku.MainApp;
 
-public class SudokuControllerTest extends Application{
+@RunWith(JUnit4.class)
+public class SudokuControllerTest {
 	SudokuController sudokuController;
 	public static MainApp mainApp;
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// noop
-		
-	}
-	
-	@BeforeClass
-	public static void initJFX() {
-	    Thread t = new Thread("JavaFX Init Thread") {
-	        public void run() {
-	            Application.launch(MainApp.class, new String[0]);
-//	        	mainApp = new MainApp();
-//	        	mainApp.main(null);
-	        }
-	    };
-	    t.setDaemon(true);
-	    t.start();
-	}
-	
+	private FxRobot robot;
+
 	@Before
-	public void setUp(){
-		mainApp = new MainApp();
+	public void setUp() throws Exception {
+		FxToolkit.registerPrimaryStage();
+		mainApp = (MainApp) FxToolkit.setupApplication(MainApp.class);
+		robot = new FxRobot(); 
 	}
 	
 	@Test
 	public void testHandleEingabe(){		
 		sudokuController = mainApp.getSudokuController();
+		sudokuController.auswahlX = 1;
+		sudokuController.auswahlY = 1;
 		sudokuController.handleEingabe(8);
 		for(int i=0; i<81; i++){
 			System.out.println(sudokuController.mapText.get(i));
 		}
+		robot.clickOn("#r01");
+		robot.type(KeyCode.DIGIT8);
 	}
 
 	
