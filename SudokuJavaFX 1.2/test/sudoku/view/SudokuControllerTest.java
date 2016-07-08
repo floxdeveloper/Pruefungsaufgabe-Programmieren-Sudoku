6516cdb3374aadb42a97917c7e8f4e5d50afe93d
floxdeveloper <fritz66@alpenjodel.de>
2016-07-08 16:21:01 +0200

    Add new Tests - finished

--- SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
+++ SudokuJavaFX 1.2/test/sudoku/view/SudokuControllerTest.java
@@ -12,8 +12,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
+import org.testfx.service.finder.impl.WindowFinderImpl;
 
+import helpElements.HelpElements;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
 import sudoku.MainApp;
 
 @RunWith(JUnit4.class)
@@ -89,6 +92,81 @@ public class SudokuControllerTest {
 		
 	}
 
+	@Test
+	public void testCongratulationDialogAfterLastSuccessfulEingabe(){
+		mainApp.getSudoku().setSudokuIfCorrect(HelpElements.testArrayCountUniqueSolvable);
+		sController.sudokuChanged();
+		
+		robot.clickOn("#r08");
+		robot.type(KeyCode.DIGIT2);
+		
+		assertEquals("Nach selbstst. Lösen öffnet sich Congratulation Dialog", "CongratulationPage", new WindowFinderImpl().listWindows().get(1).getScene().getRoot().getId());
+		
+	}
+	
+	@Test
+	public void testLockEnteredFields(){
+		sController.auswahlX = 1;
+		sController.auswahlY = 1;
+		sController.handleEingabe(8);
+		
+		sController.lockEnteredFields();
+		assertTrue("Feld 1,1 ist gesperrt", sController.editableField[1][1] == false);
+		
+		sController.auswahlX = 2;
+		sController.auswahlY = 2;
+		sController.handleEingabe(9);
+		assertTrue("Feld 2,2 ist editierbar", sController.editableField[2][2] == true);
+	}
+	
+	@Test
+	public void testSudokuChanged(){
+		mainApp.getSudoku().setSudokuIfCorrect(HelpElements.testArrayCountUniqueSolvable);
+		
+		assertEquals("Testfeld und alle anderen Felder sind leer", " ", sController.mapText.get(0).getText());
+		
+		sController.sudokuChanged();
+		
+		assertEquals("Testfeld ist 5 und alle anderen Felder sind gefüllt", "5", sController.mapText.get(0).getText());
+	}
+	
+	@Test
+	public void testUnselect(){
+		//setze das Rechteck r00 auf select
+		sController.auswahlX = 0;
+		sController.auswahlY = 0;
+		sController.mapRect.get(9 * 0 + 0).setStroke(Color.RED);
+		
+		sController.auswahlX = 0;
+		sController.auswahlY = 0;		
+		sController.unselect();
+		
+		assertEquals("Das Rechteck r00 ist abgewählt", Color.TRANSPARENT, sController.mapRect.get(0).getStroke());
+	}
+
+	@Test
+	public void testSelect(){
+		sController.auswahlX = 1;
+		sController.auswahlY = 1;
+		
+		sController.select(0, 0);
+		
+		assertEquals("Gewähltes Feld ist rot umrandet", Color.RED, sController.mapRect.get(0*9 + 0).getStroke());
+	}
+	
+	@Test
+	public void testColorAllBlack(){
+		sController.auswahlX = 1;
+		sController.auswahlY = 1;
+		sController.handleEingabe(8);
+		sController.getKoordinate(1, 1).setFill(Color.RED);
+		sController.getKoordinate(0, 0).setFill(Color.BLUE);
+		
+		sController.colorAllBlack();
+		
+		assertEquals("8 in r11 wieder schwarz, statt rot", Color.BLACK, sController.mapText.get(1*9 + 1).getFill());
+	}
+	
 	@After
 	public void clean(){
 		try {
@@ -104,6 +182,6 @@ public class SudokuControllerTest {
 			e.printStackTrace();
 		}
 	}
-	
-	
+	 
+	 
 }
