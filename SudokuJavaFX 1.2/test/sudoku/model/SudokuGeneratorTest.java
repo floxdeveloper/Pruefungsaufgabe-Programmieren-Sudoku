97f0c9f37d38c62cfa664c823c7901c4bffa7c3d
Zenty <yvette.labastille@gmail.com>
2016-07-08 09:21:16 +0200

    Add test for getNotTriedNumber

--- SudokuJavaFX 1.2/test/sudoku/model/SudokuGeneratorTest.java
+++ SudokuJavaFX 1.2/test/sudoku/model/SudokuGeneratorTest.java
@@ -3,6 +3,8 @@ package sudoku.model;
 import static helpElements.HelpElements.*;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,7 +14,6 @@ public class SudokuGeneratorTest
 	Sudoku testSudokuFilledSU;
 	Sudoku testSudokuUniqueSolvableSU;
 	Sudoku testSudokuToGenerateSU;
-	boolean[] alreadyUsed;
 	
 	
 	@Before
@@ -31,8 +32,6 @@ public class SudokuGeneratorTest
 		testSudokuFilledSU = new Sudoku(testArrayFilled);
 		testSudokuUniqueSolvableSU = new Sudoku (testArrayCountUniqueSolvable);
 		testSudokuToGenerateSU = new Sudoku(testArrayEmpty);
-		alreadyUsed = new boolean[]
-				{true, true, true, true, true, true, true, false, true};
 				
 	}
 	
@@ -41,15 +40,36 @@ public class SudokuGeneratorTest
 		assertArrayEquals("Arrays should be the same", testArrayFilled, testSudokuFilledSU.copySudokuArray());
 	}
 	
-	/*
+	
 	@Test
 	public void getNotTriedNumberShouldReturnInt()
 	{
-		int intAlreadyUsed = SudokuGenerator.getNotTriedNumber();
-		assertEquals("Should return 2", 2, intAlreadyUsed);
-		System.out.println("bla");
+		int[] alreadyUsedNumbers = new int[9];
+		
+		for (int i = 0; i < 9; i++)
+		{
+			int intAlreadyUsed = SudokuGenerator.getNotTriedNumber();
+			alreadyUsedNumbers[i] = intAlreadyUsed;
+		}
+		
+		Arrays.sort(alreadyUsedNumbers);
+		
+		assertEquals("Should return 1", 1, alreadyUsedNumbers[0]);
+		assertEquals("Should return 2", 2, alreadyUsedNumbers[1]);
+		assertEquals("Should return 3", 3, alreadyUsedNumbers[2]);
+		assertEquals("Should return 4", 4, alreadyUsedNumbers[3]);
+		assertEquals("Should return 5", 5, alreadyUsedNumbers[4]);
+		assertEquals("Should return 6", 6, alreadyUsedNumbers[5]);
+		assertEquals("Should return 7", 7, alreadyUsedNumbers[6]);
+		assertEquals("Should return 8", 8, alreadyUsedNumbers[7]);
+		assertEquals("Should return 9", 9, alreadyUsedNumbers[8]);
+		
+		for (int i = 0; i<9; i++)
+		{
+			assertFalse("10 should not appear", 10 == alreadyUsedNumbers[i]);
+		}
 	}
-	*/
+	
 	
 	@Test
 	public void generateShouldReturnSudoku()
@@ -62,6 +82,7 @@ public class SudokuGeneratorTest
 		
 		testSudokuToGenerateSU = SudokuGenerator.generate(0);
 		assertEquals("Should return true because empty", true, testSudokuToGenerateSU.empty());
+		
 	}
 	
 	
